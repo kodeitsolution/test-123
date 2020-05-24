1ec669f1e60eee2be8de9887df597b08a4b1984d
Sushil333 <sushilbhardwaj705@gmail.com>
2020-05-24 20:27:33 +0530

    Update

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('select').formSelect();
+    $('.datepicker').datepicker();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
